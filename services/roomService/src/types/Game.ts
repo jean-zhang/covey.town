0c9b949f630476f049294fd51679824d4539c3b7
kaseyl <42978150+kaseyl@users.noreply.github.com>
2021-03-28 00:05:34 -0400

    Game logic (#11)
    * implement game logic
    
    * test changes
    
    * comment out game test and remove duplicate
    
    * delete gameplayer and add dummy test
    
    * clean up comments
    
    * changes for jean's review
    
    Co-authored-by: Kasey <lee.kase@northeastern.edu>

--- services/roomService/src/types/Game.ts
+++ services/roomService/src/types/Game.ts
@@ -1,7 +1,15 @@
+import { QueryResult } from 'pg';
 import { PlayerInfo } from '../CoveyTypes';
+import pool from '../dbconnector/pool';
+import { insertMazeCompletionTime } from '../utils/queries';
+
+type finishedPlayer = {
+  player: PlayerInfo;
+  score: number;
+}
 
 /**
- * Two GamePlayers competing in the Maze is represented by a Game object
+ * Two Players competing in the Maze is represented by a Game object
  */
 export default class Game {
 
@@ -10,16 +18,35 @@ export default class Game {
   private _player2ID: string;
 
   /** The winner and loser in this Game * */
-  private _pair: Map<PlayerInfo, number>;
+  private _pair: finishedPlayer[];
 
   constructor(player1ID: string, player2ID: string) {
     this._player1ID = player1ID;
     this._player2ID = player2ID;
-    this._pair = new Map();
+    this._pair = [];
+  }
+
+  getGameId(): string {
+    return this._player1ID + this._player2ID;
+  }
+  
+  async updateScore(playerID: PlayerInfo, score: number) {
+    this._pair.push({ player: playerID, score: score});
+    if (this._pair.length >= 2) {
+      await this.registerScore();
+    }
   }
 
   /**
    * Registers the Winner's and Loser's scores 
    */ 
-  // registerScore(): void {}
+  async registerScore() {
+    const promises: Promise<QueryResult>[] = [];
+    this._pair.forEach(finishedPlayer => {
+      if (finishedPlayer.player !== undefined && finishedPlayer.score !== undefined && finishedPlayer.score > 0) {
+        promises.push(pool.query(insertMazeCompletionTime, [finishedPlayer.player.userID, finishedPlayer.player.userName, finishedPlayer.score]));
+      }
+    });
+    await Promise.all(promises);
+  }
 }
