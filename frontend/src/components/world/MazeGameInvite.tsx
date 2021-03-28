11934411f79890e509a8924fe77b111908e08a73
Jean Zhang <jean.nmn.zhang@gmail.com>
2021-03-28 16:59:17 -0400

    Formatting

--- frontend/src/components/world/MazeGameInvite.tsx
+++ frontend/src/components/world/MazeGameInvite.tsx
@@ -1,5 +1,5 @@
+import { Button, SimpleGrid, useToast } from '@chakra-ui/react';
 import React, { ReactNode } from 'react';
-import { useToast, Button, SimpleGrid } from '@chakra-ui/react';
 
 export default function MazeGameInvite(): JSX.Element {
   const INVITING_PLAYER = 'player1'; // TODO: This is just a placeholder, remove when invite logic gets added
@@ -14,22 +14,28 @@ export default function MazeGameInvite(): JSX.Element {
   function rejectRace() {
     toast.close(TOAST_ID);
     toast({
-      title: "Race rejected",
-      description: "Maybe next time :(",
-      status: "info",
+      title: 'Race rejected',
+      description: 'Maybe next time :(',
+      status: 'info',
       duration: 2000,
-      isClosable: true
+      isClosable: true,
     });
     // TODO: add logic to inform other player that the race has been rejected
   }
 
   function makeButtons(): ReactNode {
-    return (<>
-      <SimpleGrid columns={2} spacing={10}>
-        <Button colorScheme="blackAlpha" onClick={acceptRace}>accept</Button>
-        <Button colorScheme="whiteAlpha" onClick={rejectRace}>reject</Button>
-      </SimpleGrid>
-    </>)
+    return (
+      <>
+        <SimpleGrid columns={2} spacing={10}>
+          <Button colorScheme='blackAlpha' onClick={acceptRace}>
+            accept
+          </Button>
+          <Button colorScheme='whiteAlpha' onClick={rejectRace}>
+            reject
+          </Button>
+        </SimpleGrid>
+      </>
+    );
   }
 
   // TODO: currently using a button to show the toast message, remove when invite logic gets added
@@ -42,9 +48,8 @@ export default function MazeGameInvite(): JSX.Element {
           description: makeButtons(),
           duration: null,
         })
-      }
-    >
+      }>
       Show Toast
     </Button>
-  )
+  );
 }
